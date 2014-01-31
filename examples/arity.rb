869ff5a4127ac2d7549049cd5bcf772eb322f13e
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-01-30 20:13:06 -0700

    Multi arity example

---
+++ examples/arity.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../../lib/grammar', __FILE__)
+
+env = {
+    'add' => -> (*n) { n.reduce(:+) },
+    'foo' => 1,
+    'bar' => 5,
+    'baz' => 20
+}
+
+puts Grammar.process(<<-RUBY, env).value
+add(foo, bar, baz)
+RUBY
